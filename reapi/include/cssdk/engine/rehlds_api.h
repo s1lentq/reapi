f9855c55dd8a1a133725484c510258c97587f478
DarthMan <darth010@hotmail.com>
2021-10-22 21:00:31 +0300

    Update rehlds_api.h

--- reapi/include/cssdk/engine/rehlds_api.h
+++ reapi/include/cssdk/engine/rehlds_api.h
@@ -35,7 +35,7 @@
 #include "model.h"
 
 #define REHLDS_API_VERSION_MAJOR 3
-#define REHLDS_API_VERSION_MINOR 0
+#define REHLDS_API_VERSION_MINOR 11
 
 //Steam_NotifyClientConnect hook
 typedef IHookChain<qboolean, IGameClient*, const void*, unsigned int> IRehldsHook_Steam_NotifyClientConnect;
@@ -209,6 +209,18 @@ typedef IHookChainRegistry<bool, IGameClient *, bool> IRehldsHookRegistry_SV_Sho
 typedef IHookChain<struct entvars_s *, char *> IRehldsHook_GetEntityInit;
 typedef IHookChainRegistry<struct entvars_s *, char *> IRehldsHookRegistry_GetEntityInit;
 
+//SV_EmitPings hook
+typedef IHookChain<void, IGameClient *, sizebuf_t *> IRehldsHook_SV_EmitPings;
+typedef IHookChainRegistry<void, IGameClient *, sizebuf_t *> IRehldsHookRegistry_SV_EmitPings;
+
+//ED_Alloc hook
+typedef IHookChain<edict_t *> IRehldsHook_ED_Alloc;
+typedef IHookChainRegistry<edict_t *> IRehldsHookRegistry_ED_Alloc;
+
+//ED_Free hook
+typedef IVoidHookChain<edict_t *> IRehldsHook_ED_Free;
+typedef IVoidHookChainRegistry<edict_t *> IRehldsHookRegistry_ED_Free;
+
 class IRehldsHookchains {
 public:
 	virtual ~IRehldsHookchains() { }
@@ -256,6 +268,9 @@ public:
 	virtual IRehldsHookRegistry_SV_Frame* SV_Frame() = 0;
 	virtual IRehldsHookRegistry_SV_ShouldSendConsistencyList* SV_ShouldSendConsistencyList() = 0;
 	virtual IRehldsHookRegistry_GetEntityInit* GetEntityInit() = 0;
+	virtual IRehldsHookRegistry_SV_EmitPings* SV_EmitPings() = 0;
+	virtual IRehldsHookRegistry_ED_Alloc* ED_Alloc() = 0;
+	virtual IRehldsHookRegistry_ED_Free* ED_Free() = 0;
 };
 
 struct RehldsFuncs_t {
@@ -307,6 +322,65 @@ struct RehldsFuncs_t {
 	bool(*SV_EmitSound2)(edict_t *entity, IGameClient *receiver, int channel, const char *sample, float volume, float attenuation, int flags, int pitch, int emitFlags, const float *pOrigin);
 	void(*SV_UpdateUserInfo)(IGameClient *pGameClient);
 	bool(*StripUnprintableAndSpace)(char *pch);
+	void(*Cmd_RemoveCmd)(const char *cmd_name);
+	void(*GetCommandMatches)(const char *string, ObjectList *pMatchList);
+	bool(*AddExtDll)(void *hModule);
+	void(*AddCvarListener)(const char *var_name, cvar_callback_t func);
+	void(*RemoveExtDll)(void *hModule);
+	void(*RemoveCvarListener)(const char *var_name, cvar_callback_t func);
+	ENTITYINIT(*GetEntityInit)(char *pszClassName);
+
+	// Read functions
+	int(*MSG_ReadChar)();
+	int(*MSG_ReadByte)();
+	int(*MSG_ReadLong)();
+	float(*MSG_ReadFloat)();
+	char*(*MSG_ReadString)();
+	char*(*MSG_ReadStringLine)();
+	float(*MSG_ReadAngle)();
+	float(*MSG_ReadHiresAngle)();
+	void(*MSG_ReadUsercmd)(struct usercmd_s *to, struct usercmd_s *from);
+	float(*MSG_ReadCoord)();
+	void(*MSG_ReadVec3Coord)(sizebuf_t *sb, vec3_t fa);
+
+	// Read bit functions
+	bool(*MSG_IsBitReading)();
+	void(*MSG_StartBitReading)(sizebuf_t *buf);
+	void(*MSG_EndBitReading)(sizebuf_t *buf);
+	uint32(*MSG_PeekBits)(int numbits);
+	int(*MSG_ReadOneBit)();
+	uint32(*MSG_ReadBits)(int numbits);
+	int(*MSG_ReadSBits)(int numbits);
+	float(*MSG_ReadBitCoord)();
+	void(*MSG_ReadBitVec3Coord)(vec_t *fa);
+	float(*MSG_ReadBitAngle)(int numbits);
+	int(*MSG_ReadBitData)(void *dest, int length);
+	char*(*MSG_ReadBitString)();
+	int(*MSG_CurrentBit)();
+
+	// Write functions
+	void(*MSG_WriteLong)(sizebuf_t *sb, int c);
+	void(*MSG_WriteFloat)(sizebuf_t *sb, float f);
+	void(*MSG_WriteAngle)(sizebuf_t *sb, float f);
+	void(*MSG_WriteHiresAngle)(sizebuf_t *sb, float f);
+	void(*MSG_WriteUsercmd)(sizebuf_t *sb, struct usercmd_s *to, struct usercmd_s *from);
+	void(*MSG_WriteCoord)(sizebuf_t *sb, float f);
+	void(*MSG_WriteVec3Coord)(sizebuf_t *sb, const vec3_t fa);
+
+	// Write bit functions
+	bool(*MSG_IsBitWriting)();
+	void(*MSG_WriteOneBit)(int nValue);
+	void(*MSG_WriteSBits)(uint32 data, int numbits);
+	void(*MSG_WriteBitCoord)(float f);
+	void(*MSG_WriteBitAngle)(float fAngle, int numbits);
+	void(*MSG_WriteBitData)(void *src, int length);
+	void(*MSG_WriteBitString)(const char *p);
+	void(*SZ_Write)(sizebuf_t *buf, const void *data, int length);
+	void(*SZ_Print)(sizebuf_t *buf, const char *data);
+	void(*SZ_Clear)(sizebuf_t *buf);
+	void(*MSG_BeginReading)();
+	double(*GetHostFrameTime)();
+	struct cmd_function_s *(*GetFirstCmdFunctionHandle)();
 };
 
 class IRehldsApi {
