9934185924d21798819483952f82d4f16902174b
DarthMan <darth010@hotmail.com>
2021-10-22 21:04:42 +0300

    Update hook_list.cpp

--- reapi/src/hook_list.cpp
+++ reapi/src/hook_list.cpp
@@ -88,7 +88,9 @@ hook_t hooklist_engine[] = {
 	ENG(Cvar_DirectSet),
 	ENG(SV_WriteFullClientUpdate, _AMXX),
 	ENG(GetEntityInit),
-	ENG(ClientConnected)
+	ENG(ClientConnected),
+	ENG(ED_Alloc),
+	ENG(ED_Free)
 };
 
 #define DLL(h,...) { {}, {}, #h, "ReGameDLL", [](){ return api_cfg.hasReGameDLL(); }, ((!(RG_##h & (MAX_REGION_RANGE - 1)) ? regfunc::current_cell = 1, true : false) || (RG_##h & (MAX_REGION_RANGE - 1)) == regfunc::current_cell++) ? regfunc(h##__VA_ARGS__) : regfunc(#h#__VA_ARGS__), [](){ g_ReGameHookchains->h()->registerHook(&h); }, [](){ g_ReGameHookchains->h()->unregisterHook(&h); }, false}
