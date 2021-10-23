efcc3952b5d2a96280521d98d1ec6072c5d39175
Franco Romaniello <romax.cs@gmail.com>
2021-10-23 12:43:28 +0200

    Implement `rh_get_net_from()` native (#221)
    * Implement native rh_get_net_from
    
    * Add ws2_32.lib
    
    * Add ws2_32.lib in release
    
    * newline at end of reapi_utils.cpp
    
    * Update reapi_utils.cpp
    
    * fix end of file
    
    Co-authored-by: Sergey Shorokhov <wopox1337@ya.ru>

--- reapi/src/reapi_utils.h
+++ reapi/src/reapi_utils.h
@@ -59,4 +59,6 @@ void RemoveOrDropItem(CBasePlayer *pPlayer, CBasePlayerItem *pItem, GiveType typ
 
 const char *getATypeStr(AType type);
 
+char *NET_AdrToString(const netadr_t& a);
+
 extern void NORETURN UTIL_SysError(const char *fmt, ...);
