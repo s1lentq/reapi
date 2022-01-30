fe6150a5d3c34dcec980d628502d87eb207d9c86
s1lentq <s1lentsk@yandex.ru>
2022-01-31 04:25:31 +0700

    Implemented native REU_GetAuthKey

--- reapi/src/natives/natives_reunion.cpp
+++ reapi/src/natives/natives_reunion.cpp
@@ -37,6 +37,36 @@ cell AMX_NATIVE_CALL REU_GetAuthtype(AMX *amx, cell *params)
 }
 
 /*
+* Get client authkey
+*
+* @param index  Client index
+* @param index  Buffer to copy the authkey
+* @param index  Maximum buffer size
+*
+* @return       Number of cells copied to buffer
+*
+* native REU_GetAuthKey(const index, dest[], maxlen);
+*/
+cell AMX_NATIVE_CALL REU_GetAuthKey(AMX *amx, cell *params)
+{
+	enum args_e { arg_count, arg_index, arg_output, arg_maxlen };
+
+	CHECK_ISPLAYER(arg_index);
+
+	int clientId = params[arg_index] - 1;
+
+	char buffer[256];
+	size_t size = g_ReunionApi->GetClientAuthdata(clientId, buffer, sizeof buffer);
+	if (size <= 0)
+		return 0;
+
+	size_t numToCopy = min<size_t>(size, params[arg_maxlen]);
+	cell *dest = getAmxAddr(amx, params[arg_output]);
+	setAmxString(dest, buffer, numToCopy);
+	return numToCopy;
+}
+
+/*
 * Check if the client is running RevEmu with limited user rights.
 *
 * @param index  Client index
@@ -70,6 +100,7 @@ AMX_NATIVE_INFO Reunion_Natives[] =
 {
 	{ "REU_GetProtocol",                REU_GetProtocol                },
 	{ "REU_GetAuthtype",                REU_GetAuthtype                },
+	{ "REU_GetAuthKey",                 REU_GetAuthKey                 },
 	{ "REU_IsRevemuWithoutAdminRights", REU_IsRevemuWithoutAdminRights },
 
 	{ nullptr, nullptr }
