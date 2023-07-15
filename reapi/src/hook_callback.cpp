1cd9a140cc6260f3c098a867582659d69a256b20
Dmitry Novikov <s1lentsk@yandex.ru>
2023-07-16 02:33:37 +0700

    Update hook_callback.cpp

--- reapi/src/hook_callback.cpp
+++ reapi/src/hook_callback.cpp
@@ -817,7 +817,7 @@ void PM_LadderMove(IReGameHook_PM_LadderMove *chain, physent_t *pLadder)
 {
 	PM_LadderMove_args_t args(pLadder);
 	Phys_T data(chain, args);
-	PM_LadderMove_AMXX(&data, pLadder-> player + 1);
+	PM_LadderMove_AMXX(&data, pLadder->player + 1);
 }
 
 BOOL CSGameRules_FShouldSwitchWeapon(IReGameHook_CSGameRules_FShouldSwitchWeapon *chain, CBasePlayer *pPlayer, CBasePlayerItem *pWeapon)
