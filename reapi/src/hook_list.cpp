6e72bb606be1fac66895aa71b1af74dc25f3f591
Natsheh11 <65265026+Natsheh11@users.noreply.github.com>
2024-03-30 02:01:26 +0300

    Update hook_list.cpp

--- reapi/src/hook_list.cpp
+++ reapi/src/hook_list.cpp
@@ -25,6 +25,7 @@ inline size_t getFwdParamType(void(*)(resourcetype_t))          { return FP_CELL
 inline size_t getFwdParamType(void(*)(cmd_source_t))            { return FP_CELL;   }
 inline size_t getFwdParamType(void(*)(GameEventType))           { return FP_CELL;   }
 inline size_t getFwdParamType(void(*)(float*))                  { return FP_ARRAY;  }
+inline size_t getFwdParamType(void(*)(const float*))            { return FP_ARRAY;  }
 
 template <typename T>
 inline size_t getFwdParamType(void(*)(T *))                     { return FP_CELL;   }
